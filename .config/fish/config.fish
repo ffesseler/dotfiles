1644eafc1b2d6b3d3934f35df86c11c9ca84de45
Florian Fesseler <ffesseler@kapit.fr>
2016-11-09 15:36:33 +0100

    remove nvm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,5 @@ set -x MAVEN_OPTS '-Xmx1024m -Xms512m -XX:MaxPermSize=256m'
 set -x BUILD_NUMBER 99999
 set -gx PATH $PATH ./node_modules/.bin
 set -x JENV_ROOT /usr/local/opt/jenv
-set -x NVM_DIR ~/.nvm
-source ~/.config/fish/nvm-wrapper/nvm.fish
 if test -f /Users/ffesseler/.autojump/share/autojump/autojump.fish; . /Users/ffesseler/.autojump/share/autojump/autojump.fish; end
 . $HOME/.config/fish/frontend-aliases.fish
