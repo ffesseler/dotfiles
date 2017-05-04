67e80e8c7ad23af54d6fc9535148c228ee494e77
Florian Fesseler <ffesseler@kapit.fr>
2017-05-04 12:14:58 +0200

    add JAVA_7_HOME in path and postreg related path variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,12 @@
 . $HOME/.config/fish/solarized.fish
 set -x JAVA_HOME (/usr/libexec/java_home -v1.7)
+set -x JAVA_7_HOME (/usr/libexec/java_home -v1.7)
 set -x MAVEN_OPTS '-Xmx1024m -Xms512m -XX:MaxPermSize=256m'
 set -x BUILD_NUMBER 99999
 set -gx PATH $PATH ./node_modules/.bin
+set -gx PATH $PATH /Applications/Postgres.app/Contents/Versions/latest/bin
 set -x JENV_ROOT /usr/local/opt/jenv
+set -x DATABASE_URL postgres:///(whoami)
+
 if test -f /Users/ffesseler/.autojump/share/autojump/autojump.fish; . /Users/ffesseler/.autojump/share/autojump/autojump.fish; end
 . $HOME/.config/fish/frontend-aliases.fish
