9e893e98c1be87ad29198d6d90675409bce8719a
Florian Fesseler <ffesseler@Emerics-MacBook-Pro-2.local>
2025-07-02 13:29:49 +0200

    update Brewfile & brew installation
    Restarting a Brewfile from scratch

--- install.sh
+++ install.sh
@@ -6,12 +6,11 @@ echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update Homebrew recipes
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
 brew bundle
