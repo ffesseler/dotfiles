b1f6d37937c83b5fc77d7f16a6bf96c313891e46
Florian Fesseler <ffesseler@Emerics-MacBook-Pro-2.local>
2025-07-02 14:57:32 +0200

    change Spotlight shortcut

--- osx.sh
+++ osx.sh
@@ -5,6 +5,12 @@
 # Ask for the administrator password upfront
 sudo -v
 
+# Disable the current Spotlight shortcut (usually Cmd+Space)
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "<dict><key>enabled</key><false/></dict>"
+
+# Set Option+Space for Spotlight
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "<dict><key>enabled</key><true/><key>value</key><dict><key>parameters</key><array><integer>32</integer><integer>49</integer><integer>524288</integer></array><key>type</key><string>standard</string></dict></dict>"
+
 # Dock
 defaults write com.apple.dock wvous-tl-corner -int 13
 killall Dock
