22d8cb88ab009b4833d75a14d94d7cd7a210b0ec
Florian Fesseler <ffesseler@Emerics-MacBook-Pro-2.local>
2025-07-02 16:43:50 +0200

    add asdf

--- Brewfile
+++ Brewfile
@@ -1,3 +1,5 @@
+brew 'asdf'
+
 # Apps
 cask 'slack'
 cask 'google-chrome'
--- dev_env.sh
+++ dev_env.sh
@@ -1,29 +1,5 @@
 #!/bin/sh
 
-# vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir ~/.vim/colors
-cp colors/Tomorrow-Night.vim ../.vim/colors/
-cp colors/solarized.vim ../.vim/colors/
-vim +PlugInstall +qall
-ln -s ~/dotfiles/cheat40.txt ~/.vim
-
-# tmux
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-wget -O - http://git.io/tmux-up > /usr/local/bin/tmux-up
-chmod u+x /usr/local/bin/tmux-up
-
-cp dev2.conf ../dev2.conf
-mkdir ~/.m2
-cp settings.xml ~/.m2/
-cp toolchains.xml ~/.m2/
-
-# fish
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
-rm -rf ~/.config/fish
-ln -s ~/dotfiles/.config/fish ~/.config
-
-# infinite undo vim
-mkdir ~/.vim/undodir
+asdf plugin add nodejs
+asdf install nodejs 20.11.1
+asdf set -u nodejs 20.11.1
